7381a9e5dc069f68ac32982e05ffffc08ba4ce49
max konovalenko <konoval2001enkoxbox@gmail.com>
2025-10-16 21:50:27 +0300

    вроде придумал запуск

--- FirstApp.View/Platforms/Android/MainActivity.cs
+++ FirstApp.View/Platforms/Android/MainActivity.cs
@@ -1,11 +1,19 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using static AndroidX.Core.SplashScreen.SplashScreen;
 
 namespace FirstApp.View
 {
+    //[Activity(Theme = "@style/Theme.Animated", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        //protected override void OnCreate(Bundle? savedInstanceState)
+        //{
+        //    var splash = InstallSplashScreen(this);
+        //    base.OnCreate(savedInstanceState);
+
+        //}
     }
 }
