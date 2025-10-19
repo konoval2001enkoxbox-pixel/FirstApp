56172665b19b80b03dc443b0d541626a5f2b75b7
max konovalenko <konoval2001enkoxbox@gmail.com>
2025-10-19 23:37:39 +0300

    скролер!!!!

---
+++ FirstApp.View/SwipeDirection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+public enum SwipeDirection
+{
+    None,
+    LeftToRight,
+    RightToLeft,
+    TopToBottom,
+    BottomToTop
+}
--- FirstApp.View/wwwroot/js/main.js
+++ FirstApp.View/wwwroot/js/main.js
@@ -1,1 +1,4 @@
-﻿
+﻿window.calculateTransformAndApply = (scroller, currentIndex, direction) => {
+    const translateX = -currentIndex * 100 + (direction / window.innerWidth * 100); // Calculate translation
+    scroller.style.transform = `translateX(${translateX}%)`; // Apply the transformation
+};
